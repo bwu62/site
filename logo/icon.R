fa903ddb09558919fbddc6fe2b718c3cc057a49e
Bi Cheng Wu <bwu62@wisc.edu>
2021-04-14 23:17:14 -0500

    added favicons

--- logo/icon.R
+++ logo/icon.R
@@ -12,7 +12,7 @@ Nightingale %>%
   gather("cause","radius",2:3) %>% 
   mutate_at("radius",{function(.)./max(.)}) %>% 
   mutate(radius = ifelse(Month %in% c("Apr 1854","May 1854","Jun 1854"),
-                         radius*1.3,radius)) %>% 
+                         radius*1.4,radius)) %>% 
   ggplot(aes(x=Month,y=radius,fill=cause,order=cause)) + 
   geom_bar(stat="identity",position="identity",width=1,color="yellow",size=1.5) + 
   scale_fill_manual(values=c("red","black")) + 
@@ -21,19 +21,16 @@ Nightingale %>%
         line=element_blank(),rect=element_blank(),
         text=element_blank(),title=element_blank(),
         plot.background=element_rect(fill="yellow",colour="yellow"))
-ggsave("coxcomb.svg",width=6)
+ggsave("coxcomb.svg",width=6,height=6)
 
 ggplot() + 
-  geom_regon(aes(x0=0,y0=0,r=1,sides=6,angle=0),fill="yellow",color="black",size=12) + 
-  coord_fixed() + 
-  theme_void()
-ggsave("hex.svg",bg="transparent",width=6)
+  geom_regon(aes(x0=0,y0=0,r=1,sides=6,angle=0),fill="yellow",color="black",size=12) +
+  coord_fixed(ratio=1) + theme_void()
+ggsave("hex.svg",bg="transparent",width=6,height=6)
 
-(logo = image_composite(
-  image_trim(image_read("hex.svg",density=400)),
-  image_trim(image_transparent(image_read("coxcomb.svg",density=420),color="yellow")),
-  offset="+460+290"
-))
+(hex = image_read("hex.svg",density=600))
+(coxcomb = image_trim(image_transparent(image_read("coxcomb.svg",density=600),color="yellow")))
+offset = paste0(sprintf("%+1d",as.numeric(round(c(-.03,.02)*image_info(coxcomb)[c(2,3)]))),collapse="")
+( logo = image_composite(hex,coxcomb,gravity="Center",offset=offset) )
 
-image_write(image_transparent(logo,color="white"),
-            path="logo.png",format="png",density=1000)
+image_write(image_transparent(logo,color="white"),path="logo.png",format="png",density=1000)
